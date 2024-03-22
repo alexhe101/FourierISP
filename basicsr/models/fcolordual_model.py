1ba8ede80f2ade70ca249b2b2733341e803576bf
Hsüan hua <46086948+alexhe101@users.noreply.github.com>
2024-03-22 12:58:15 +0800

    Add files via upload

---
+++ basicsr/models/fcolordual_model.py
@@ -0,0 +1,326 @@
+import torch
+from collections import OrderedDict
+from os import path as osp
+from tqdm import tqdm
+
+from basicsr.archs import build_network
+from basicsr.losses import build_loss
+from basicsr.metrics import calculate_metric
+from basicsr.utils import get_root_logger, imwrite, tensor2img
+from basicsr.utils.registry import MODEL_REGISTRY
+from .base_model import BaseModel
+from torch import nn
+
+@MODEL_REGISTRY.register()
+class FcolorDualModel(BaseModel):
+    """Base SR model for single image super-resolution."""
+
+    def __init__(self, opt):
+        super(FcolorDualModel, self).__init__(opt)
+
+        # define network
+        self.net_g = build_network(opt['network_g'])
+        self.net_g = self.model_to_device(self.net_g)
+        self.print_network(self.net_g)
+        # load pretrained models
+        load_path = self.opt['path'].get('pretrain_network_g', None)
+        if load_path is not None:
+            param_key = self.opt['path'].get('param_key_g', 'params')
+            self.load_network(self.net_g, load_path, self.opt['path'].get('strict_load_g', True), param_key)
+        self.gclip = -1
+        if self.is_train:
+            self.gclip = self.opt['train']['gclip']
+            self.init_training_settings()
+
+    def init_training_settings(self):
+        self.net_g.train()
+        train_opt = self.opt['train']
+        self.ema_decay = train_opt.get('ema_decay', 0)
+        if self.ema_decay > 0:
+            logger = get_root_logger()
+            logger.info(f'Use Exponential Moving Average with decay: {self.ema_decay}')
+            # define network net_g with Exponential Moving Average (EMA)
+            # net_g_ema is used only for testing on one GPU and saving
+            # There is no need to wrap with DistributedDataParallel
+            self.net_g_ema = build_network(self.opt['network_g']).to(self.device)
+            # load pretrained model
+            load_path = self.opt['path'].get('pretrain_network_g', None)
+            if load_path is not None:
+                self.load_network(self.net_g_ema, load_path, self.opt['path'].get('strict_load_g', True), 'params_ema')
+            else:
+                self.model_ema(0)  # copy net_g weight
+            self.net_g_ema.eval()
+
+        if train_opt.get('pixel_opt'):
+            self.cri_pix = build_loss(train_opt['pixel_opt']).to(self.device)
+        else:
+            self.cri_pix = None
+        if train_opt.get('ssim_opt'):
+            self.cri_ssim = build_loss(train_opt['ssim_opt']).to(self.device)
+        else:
+            self.cri_ssim = None
+        if train_opt.get('perceptual_opt'):
+            self.cri_perceptual = build_loss(train_opt['perceptual_opt']).to(self.device)
+        else:
+            self.cri_perceptual = None
+        if train_opt.get('vgg_opt'):
+            self.cri_vgg = build_loss(train_opt['vgg_opt']).to(self.device)
+        else:
+            self.cri_vgg = None
+        if self.cri_pix is None and self.cri_perceptual is None:
+            raise ValueError('Both pixel and perceptual losses are None.')
+
+        if train_opt.get('fft_opt'):
+            self.cri_fft = build_loss(train_opt['fft_opt']).to(self.device)
+        else:
+            self.cri_fft = None
+
+        if self.cri_pix is None and self.cri_fft is None:
+            raise ValueError('Both pixel and perceptual losses are None.')
+
+        # set up optimizers and schedulers
+        self.setup_optimizers()
+        self.setup_schedulers()
+
+    def setup_optimizers(self):
+        train_opt = self.opt['train']
+        optim_params = []
+        for k, v in self.net_g.named_parameters():
+            if v.requires_grad:
+                optim_params.append(v)
+            else:
+                logger = get_root_logger()
+                logger.warning(f'Params {k} will not be optimized.')
+
+        optim_type = train_opt['optim_g'].pop('type')
+        self.optimizer_g = self.get_optimizer(optim_type, optim_params, **train_opt['optim_g'])
+        self.optimizers.append(self.optimizer_g)
+
+    def feed_data(self, data):
+        self.combined = data['combined'].to(self.device)
+        self.demosaic = data['demosaic'].to(self.device)
+        if 'gt' in data:
+            self.gt = data['gt'].to(self.device)
+
+    def optimize_parameters(self, current_iter):
+        self.optimizer_g.zero_grad()
+        self.out_1_phase, self.out_1_amp, self.out_2 = self.net_g(self.combined,self.demosaic)
+
+        l_total = 0
+        loss_dict = OrderedDict()
+        # pixel loss
+        if self.cri_pix:
+            l_pix = self.cri_pix(self.out_2, self.gt)
+            l_total += l_pix
+            loss_dict['l_pix'] = l_pix
+        if self.cri_fft:
+            # l_fft = self.cri_fft(self.out_2,self.gt,self.out_1_amp)
+            l_fft = self.cri_fft(self.out_2, self.gt, self.out_1_phase,self.out_1_amp)
+            # l_fft = self.cri_fft(self.out_2, self.gt, self.out_1_phase,self.out_1)
+            # l_fft = self.cri_fft(self.out_2, self.gt, self.out_1_amp,self.out_1)
+
+            l_total+=l_fft
+            loss_dict['l_fft'] = l_fft
+        if self.cri_ssim:
+            l_ssim = self.cri_ssim(self.out_2, self.gt)
+            l_total+=l_ssim
+            loss_dict['l_ssim'] = l_ssim
+        if self.cri_vgg:
+            l_vgg = self.cri_vgg(self.out_2,self.gt)
+            l_total+=l_vgg
+            loss_dict['l_vgg'] = l_vgg
+        l_total.backward()
+        # if self.gclip > 0:
+        #     if self.ema_decay > 0:
+        #         nn.utils.clip_grad_norm_(
+        #             self.net_g_ema.parameters(),
+        #             self.gclip
+        #         )
+        #     else:
+        #         nn.utils.clip_grad_norm_(
+        #             self.net_g.parameters(),
+        #             self.gclip
+        #         )
+        if self.gclip > 0:
+            nn.utils.clip_grad_norm_(
+                self.net_g.parameters(),
+                self.gclip
+            )
+
+        self.optimizer_g.step()
+        self.log_dict = self.reduce_loss_dict(loss_dict)
+
+        if self.ema_decay > 0:
+            self.model_ema(decay=self.ema_decay)
+    def test(self):
+        if hasattr(self, 'net_g_ema'):
+            self.net_g_ema.eval()
+            with torch.no_grad():
+                self.out_1_phase, self.out_1_amp, self.out_2 = self.net_g_ema(self.combined,self.demosaic)
+        else:
+            self.net_g.eval()
+            with torch.no_grad():
+                self.out_1_phase, self.out_1_amp, self.out_2 = self.net_g(self.combined,self.demosaic)
+            self.net_g.train()
+
+    # def test_selfensemble(self):
+    #     # TODO: to be tested
+    #     # 8 augmentations
+    #     # modified from https://github.com/thstkdgus35/EDSR-PyTorch
+    #
+    #     def _transform(v, op):
+    #         # if self.precision != 'single': v = v.float()
+    #         v2np = v.data.cpu().numpy()
+    #         if op == 'v':
+    #             tfnp = v2np[:, :, :, ::-1].copy()
+    #         elif op == 'h':
+    #             tfnp = v2np[:, :, ::-1, :].copy()
+    #         elif op == 't':
+    #             tfnp = v2np.transpose((0, 1, 3, 2)).copy()
+    #
+    #         ret = torch.Tensor(tfnp).to(self.device)
+    #         # if self.precision == 'half': ret = ret.half()
+    #
+    #         return ret
+    #
+    #     # prepare augmented data
+    #     lq_list = [self.lq]
+    #     for tf in 'v', 'h', 't':
+    #         lq_list.extend([_transform(t, tf) for t in lq_list])
+    #
+    #     # inference
+    #     if hasattr(self, 'net_g_ema'):
+    #         self.net_g_ema.eval()
+    #         with torch.no_grad():
+    #             out_list = [self.net_g_ema(aug) for aug in lq_list]
+    #     else:
+    #         self.net_g.eval()
+    #         with torch.no_grad():
+    #             out_list = [self.net_g_ema(aug) for aug in lq_list]
+    #         self.net_g.train()
+    #
+    #     # merge results
+    #     for i in range(len(out_list)):
+    #         if i > 3:
+    #             out_list[i] = _transform(out_list[i], 't')
+    #         if i % 4 > 1:
+    #             out_list[i] = _transform(out_list[i], 'h')
+    #         if (i % 4) % 2 == 1:
+    #             out_list[i] = _transform(out_list[i], 'v')
+    #     output = torch.cat(out_list, dim=0)
+    #
+    #     self.output = output.mean(dim=0, keepdim=True)
+
+    def dist_validation(self, dataloader, current_iter, tb_logger, save_img):
+        if self.opt['rank'] == 0:
+            self.nondist_validation(dataloader, current_iter, tb_logger, save_img)
+
+    def nondist_validation(self, dataloader, current_iter, tb_logger, save_img):
+        dataset_name = dataloader.dataset.opt['name']
+        with_metrics = self.opt['val'].get('metrics') is not None
+        use_pbar = self.opt['val'].get('pbar', False)
+
+        if with_metrics:
+            if not hasattr(self, 'metric_results'):  # only execute in the first run
+                self.metric_results = {metric: 0 for metric in self.opt['val']['metrics'].keys()}
+            # initialize the best metric results for each dataset_name (supporting multiple validation datasets)
+            self._initialize_best_metric_results(dataset_name)
+        # zero self.metric_results
+        if with_metrics:
+            self.metric_results = {metric: 0 for metric in self.metric_results}
+
+        metric_data = dict()
+        if use_pbar:
+            pbar = tqdm(total=len(dataloader), unit='image')
+
+        for idx, val_data in enumerate(dataloader):
+            img_name = osp.splitext(osp.basename(val_data['gt_path'][0]))[0]
+            self.feed_data(val_data)
+            self.test()
+
+            visuals = self.get_current_visuals()
+            sr_img = tensor2img([visuals['result']])
+            # lq_img = tensor2img([visuals['lq']])
+            metric_data['img'] = sr_img
+            if 'gt' in visuals:
+                gt_img = tensor2img([visuals['gt']])
+                metric_data['img2'] = gt_img
+                del self.gt
+
+            # tentative for out of GPU memory
+            del self.combined
+            del self.demosaic
+            del self.out_2
+            del self.out_1_amp
+            del self.out_1_phase
+            torch.cuda.empty_cache()
+
+            if save_img:
+                if self.opt['is_train']:
+                    save_img_path = osp.join(self.opt['path']['visualization'], img_name,
+                                             f'{img_name}_{current_iter}.png')
+                    # lq_img_path = osp.join(self.opt['path']['visualization'], img_name,
+                    #                          f'{img_name}_{current_iter}_lq.png')
+                else:
+                    if self.opt['val']['suffix']:
+                        save_img_path = osp.join(self.opt['path']['visualization'], dataset_name,
+                                                 f'{img_name}_{self.opt["val"]["suffix"]}.png')
+                        # lq_img_path = osp.join(self.opt['path']['visualization'], dataset_name,
+                        #                          f'{img_name}_{self.opt["val"]["suffix"]}_lq.png')
+
+                    else:
+                        save_img_path = osp.join(self.opt['path']['visualization'], dataset_name,
+                                                 f'{img_name}_{self.opt["name"]}.png')
+                        # lq_img_path = osp.join(self.opt['path']['visualization'], dataset_name,
+                        #                          f'{img_name}_{self.opt["name"]}_lq.png')
+
+                # imwrite(lq_img,lq_img_path)
+                imwrite(sr_img, save_img_path)
+
+            if with_metrics:
+                # calculate metrics
+                for name, opt_ in self.opt['val']['metrics'].items():
+                    self.metric_results[name] += calculate_metric(metric_data, opt_)
+            if use_pbar:
+                pbar.update(1)
+                pbar.set_description(f'Test {img_name}')
+        if use_pbar:
+            pbar.close()
+
+        if with_metrics:
+            for metric in self.metric_results.keys():
+                self.metric_results[metric] /= (idx + 1)
+                # update the best metric result
+                self._update_best_metric_result(dataset_name, metric, self.metric_results[metric], current_iter)
+
+            self._log_validation_metric_values(current_iter, dataset_name, tb_logger)
+
+    def _log_validation_metric_values(self, current_iter, dataset_name, tb_logger):
+        log_str = f'Validation {dataset_name}\n'
+        for metric, value in self.metric_results.items():
+            log_str += f'\t # {metric}: {value:.4f}'
+            if hasattr(self, 'best_metric_results'):
+                log_str += (f'\tBest: {self.best_metric_results[dataset_name][metric]["val"]:.4f} @ '
+                            f'{self.best_metric_results[dataset_name][metric]["iter"]} iter')
+            log_str += '\n'
+
+        logger = get_root_logger()
+        logger.info(log_str)
+        if tb_logger:
+            for metric, value in self.metric_results.items():
+                tb_logger.add_scalar(f'metrics/{dataset_name}/{metric}', value, current_iter)
+
+    def get_current_visuals(self):
+        out_dict = OrderedDict()
+        out_dict['demosaic'] = self.demosaic.detach().cpu()
+        out_dict['result'] = self.out_2.detach().cpu()
+        if hasattr(self, 'gt'):
+            out_dict['gt'] = self.gt.detach().cpu()
+        return out_dict
+
+    def save(self, epoch, current_iter):
+        if hasattr(self, 'net_g_ema'):
+            self.save_network([self.net_g, self.net_g_ema], 'net_g', current_iter, param_key=['params', 'params_ema'])
+        else:
+            self.save_network(self.net_g, 'net_g', current_iter)
+        self.save_training_state(epoch, current_iter)
